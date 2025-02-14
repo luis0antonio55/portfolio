79f43464f8365f6c3cc1e6598228d7b43bfbfedc
luis0antonio55 <luis0antonio5@gmail.com>
2025-02-14 13:55:08 -0700

    new astro config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
+
 export default defineConfig({
-  site: 'https://luis0antonio55.github.io',
-  base: 'portfolio',
   integrations: [tailwind(), react()],
   adapter: vercel({
     imageService: true,
